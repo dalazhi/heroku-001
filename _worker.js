6f62c2719a608d61035840506397aba120ee241d
dalazhi <45230013+dalazhi@users.noreply.github.com>
2022-06-24 16:08:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,22 +1,19 @@
-/*export default {
+export default {
 async fetch(request, env) {
 const cars = [
-"dalazhi-vm.herokuapp.com",
-"dalazhi-vm01.herokuapp.com",
-"dalazhi-vm02.herokuapp.com",
-"dalazhi-vm03.herokuapp.com"
-]; 
-*/
-export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="dalazhi-vm.herokuapp.com";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
-    }
-  };
+"huku01.herokuapp.com",
+"huku02.herokuapp.com",
+"huku03.herokuapp.com"
+];
+let host = cars[Math.floor(Math.random() * cars.length)]; //随机选择VPS
+//let host = cars[new Date().getDate() % cars.length]; //每天自动更换VPS
 
+let url = new URL(request.url);
+if (url.pathname.startsWith('/')) {
+url.hostname = host;
+let new_request = new Request(url,request);
+return fetch(new_request);
+}
+return env.ASSETS.fetch(request);
+}
+};
