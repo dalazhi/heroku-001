36aeb22742ea8a7d71e33d0453db1ef0919319ce
dalazhi <45230013+dalazhi@users.noreply.github.com>
2022-07-20 11:21:42 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,8 +7,8 @@ const cars = [
 "huku05.herokuapp.com",
   "huku06.herokuapp.com"
 ];
-let host = cars[Math.floor(Math.random() * cars.length)]; //随机选择VPS
-//let host = cars[new Date().getDate() % cars.length]; //每天自动更换VPS
+//let host = cars[Math.floor(Math.random() * cars.length)]; //随机选择VPS
+let host = cars[new Date().getDate() % cars.length]; //每天自动更换VPS
 
 let url = new URL(request.url);
 if (url.pathname.startsWith('/')) {
